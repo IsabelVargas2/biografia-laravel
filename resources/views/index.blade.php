93ce14d8d3a5f6f1a2561ebcde027c6b24887406
IsabelVargas2 <mvargas859@unab.edu.co>
2025-09-13 21:41:43 -0500

    lugar de nacimiento y procedencia

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title>Mi Biografía</title>
+  <!-- CDN Bootstrap para que se vea bonito sin configurar assets -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+  <div class="container py-5">
+    <div class="card shadow-sm">
+      <div class="card-body">
+        <h1 id="nombre">María Isabel Vargas Mendoza</h1>
+        <!-- se irán agregando secciones en commits siguientes -->
+        <!-- dentro del card-body, debajo del h1 -->
+        <h3>Origen</h3>
+        <p>
+        Nací en: Bucaramanga y soy de Colombia. <br>
+        Padres: Mi papá se llama Julio Vargas y mi mamá Neyla Sanchez. <br>
+        Hermanos: No tengo.
+        </p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+
